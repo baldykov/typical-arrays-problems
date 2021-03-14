6c779dc9bbd620343eba054327a421b050f2474f
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-03-14 21:56:53 +0300

    feat: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (!array || array.length <= 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    let result = Number.MAX_SAFE_INTEGER;
+    for (const num of array) {
+        if (num < result) {
+            result = num;
+        }
+    }
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length <= 0) return 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    let result = Number.MIN_SAFE_INTEGER;
+    for (const num of array) {
+        if (num > result) {
+            result = num;
+        }
+    }
+    return result;
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length <= 0) return 0;
+    return array.reduce((acc, item) => acc + item) / array.length;
+};
